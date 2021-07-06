c7b481b10b8a6afd3c7d215f37f59a162fb64746
Matheus vieira <matheustsr@outlook.com>
2021-07-06 04:49:08 -0300

    status removed config

--- src/bills/services/remove.js
+++ src/bills/services/remove.js
@@ -2,5 +2,4 @@ import Bill from '../schema/Bill';
 
 export default (req, res) => {
     Bill.findByIdAndRemove(req.params.id).then(res.json({ status: 'removed' }));
-    Bill.findByIdAndRemove(req.params.id).then(res.json({ status: 'removed' }));
 };
